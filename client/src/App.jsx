08845ffb261db6e4d472f219550cf789a5467df0
Your Name <youremail@yourdomain.com>
2022-12-01 22:44:51 -0500

    cracks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <div>
         <h1>hola Amigos! ?</h1>
         <h1>Hola grupo que tal</h1>
+        <h1>Cracks</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
