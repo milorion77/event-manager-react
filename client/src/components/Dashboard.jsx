7fe0d879e8e0e18c027ee821abce1ed80196eb42
Cristian Vargas <crsvar@hotmail.com>
2022-12-13 18:07:45 -0500

    Automatic ref

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "@fullcalendar/react/dist/vdom";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
@@ -7,247 +7,261 @@ import * as bootstrap from "bootstrap";
 import { Link } from "react-router-dom";
 import AWN from "awesome-notifications";
 import avatar from "../img/avatars/FEMALE-STAY.gif";
+import axios from "axios";
 
 function Dashboard(props) {
   const [currentEvent, setCurrentEvent] = useState({});
   const [newEvent, setNewEvent] = useState({});
-
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/event/getAll")
+      .then((res) => {
+        const events = res.data.map((event) => ({
+          title: event.title,
+          description: event.Description,
+          type: event.Type,
+          date: `${event.Date}T${event.Time}:00`,
+        }));
+        setEvents(events);
+      });
+  }, []);
+  console.log(events)
   let notifier = new AWN({
     durations: {
-      global: 2000
-    }
+      global: 2000,
+    },
   });
   return (
-    <div >
-        <nav class="navbar navbar-dark bg-primary fixed-top">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-              Event Manager Dashboard
-            </a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="offcanvas"
-              data-bs-target="#offcanvasDarkNavbar"
-              aria-controls="offcanvasDarkNavbar"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div
-              class="offcanvas offcanvas-end text-bg-dark"
-              tabindex="-1"
-              id="offcanvasDarkNavbar"
-              aria-labelledby="offcanvasDarkNavbarLabel"
-            >
-              <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
-                  Welcome To The Evently App
-                </h5>
-                <button
-                  type="button"
-                  class="btn-close btn-close-white"
-                  data-bs-dismiss="offcanvas"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div class="offcanvas-body">
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-2">
-                  <li class="nav-item">
-                    <Link className="ms-2 float-right" to={"/add-event"}>
-                      Crear Evento
-                    </Link>
-                  </li>
-                  <li class="nav-item">
+    <div>
+      <nav class="navbar navbar-dark bg-primary fixed-top">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Event Manager Dashboard
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasDarkNavbar"
+            aria-controls="offcanvasDarkNavbar"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="offcanvas offcanvas-end text-bg-dark"
+            tabindex="-1"
+            id="offcanvasDarkNavbar"
+            aria-labelledby="offcanvasDarkNavbarLabel"
+          >
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                Welcome To The Evently App
+              </h5>
+              <button
+                type="button"
+                class="btn-close btn-close-white"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-2">
+                <li class="nav-item">
+                  <Link className="ms-2 float-right" to={"/add-event"}>
+                    Crear Evento
+                  </Link>
+                </li>
+                <li class="nav-item">
                   <Link className="ms-2 float-right" to={"/edit-event/0"}>
-                      Editar Evento
-                    </Link>
-                  </li>
-                  <li class="nav-item">
-                    <Link className="ms-2 float-right" to={"/"}>
-                      Cerrar Sesión
-                    </Link>
-                  </li>
-                </ul>
+                    Editar Evento
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link className="ms-2 float-right" to={"/"}>
+                    Cerrar Sesión
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <h1>Calendario</h1>
+      <div className="row">
+        <div className="col-10">
+          <FullCalendar
+            buttonText={{ today: "Hoy" }}
+            editable={"true"}
+            locale={"esLocale"}
+            height={"auto"}
+            plugins={[dayGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            events={events}
+            eventClick={(info) => {
+              const modal = new bootstrap.Modal("#eventModal", {
+                keyboard: false,
+              });
+              setCurrentEvent({
+                title: info.event.title,
+                date: info.event.start.toISOString().split("T")[0],
+                hour: info.event.start.toISOString().slice(11, 16),
+              });
+              modal.show();
+            }}
+            dateClick={(info) => {
+              const actual_date = new Date();
+              if (info.date < actual_date) {
+                notifier.alert("No puedes crear eventos en el pasado");
+              } else {
+                const modal = new bootstrap.Modal("#newModal", {
+                  keyboard: false,
+                });
+                setNewEvent({
+                  date: info.date.toISOString().split("T")[0],
+                });
+                modal.show();
+              }
+            }}
+          />
+          <div class="modal text-start" id="eventModal">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header text-center">
+                  <h5 class="modal-title">Evently</h5>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div class="modal-body">
+                  <form>
+                    <div className="form-group mb-3">
+                      <label>Title</label>
+                      <input
+                        type={"text"}
+                        className="form-control"
+                        value={currentEvent.title}
+                      />
+                    </div>
+                    <div className="form-group mb-3">
+                      <label>Categoría</label>
+                      <input
+                        type={"text"}
+                        className="form-control"
+                        value={"Cumpleaños"}
+                      />
+                    </div>
+                    <div className="form-group mb-3">
+                      <label>Descripción</label>
+                      <input
+                        type={"text"}
+                        className="form-control"
+                        value={"Evento de boda"}
+                      />
+                    </div>
+                    <div className="form-group mb-3">
+                      <label>Fecha</label>
+                      <input
+                        type={"date"}
+                        className="form-control"
+                        value={currentEvent.date}
+                      />
+                    </div>
+                    <div className="form-group mb-3">
+                      <label>Hora</label>
+                      <input
+                        type={"time'"}
+                        className="form-control"
+                        value={currentEvent.hour}
+                      />
+                    </div>
+                    <div className="form-group mb-3">
+                      <label>Ubicación</label>
+                      <input
+                        type={"text"}
+                        className="form-control"
+                        value={"Rionegro"}
+                      />
+                    </div>
+                    <div className="form-group mb-3">
+                      <button className="btn btn-info" type="submit">
+                        Editar
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div class="modal-footer"></div>
               </div>
             </div>
           </div>
-        </nav>
-        <h1>Calendario</h1>
-        <div className="row">
-          <div className="col-10">
-              <FullCalendar
-                buttonText={{ today: "Hoy" }}
-                editable={"true"}
-                locale={"esLocale"}
-                height={"auto"}
-                plugins={[dayGridPlugin, interactionPlugin]}
-                initialView="dayGridMonth"
-                events={[
-                  {
-                    title: "Boda de Miguel",
-                    start: "2022-12-03T14:30:00",
-                  },
-                  { title: "Boda de Sara", start: "2022-12-03T17:30:00" },
-                ]}
-                eventClick={(info) => {
-                  const modal = new bootstrap.Modal("#eventModal", {
-                    keyboard: false,
-                  });
-                  setCurrentEvent({
-                    title: info.event.title,
-                    date: info.event.start.toISOString().split("T")[0],
-                    hour: info.event.start.toISOString().slice(11, 16),
-                  });
-                  modal.show();
-                }}
-                dateClick={(info) => {
-                  const actual_date = new Date();
-                  if (info.date < actual_date) {
-                    notifier.alert('No puedes crear eventos en el pasado')
-                  } else {
-                    const modal = new bootstrap.Modal("#newModal", {
-                      keyboard: false,
-                    });
-                    setNewEvent({
-                      date: info.date.toISOString().split("T")[0],
-                    });
-                    modal.show();
-                  }
-                }}
-              />
-              <div class="modal text-start" id="eventModal">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header text-center">
-                      <h5 class="modal-title">Evently</h5>
-                      <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Close"
-                      ></button>
-                    </div>
-                    <div class="modal-body">
-                      <form>
-                        <div className="form-group mb-3">
-                          <label>Title</label>
-                          <input
-                            type={"text"}
-                            className="form-control"
-                            value={currentEvent.title}
-                          />
-                        </div>
-                        <div className="form-group mb-3">
-                          <label>Categoría</label>
-                          <input
-                            type={"text"}
-                            className="form-control"
-                            value={"Cumpleaños"}
-                          />
-                        </div>
-                        <div className="form-group mb-3">
-                          <label>Descripción</label>
-                          <input
-                            type={"text"}
-                            className="form-control"
-                            value={"Evento de boda"}
-                          />
-                        </div>
-                        <div className="form-group mb-3">
-                          <label>Fecha</label>
-                          <input
-                            type={"date"}
-                            className="form-control"
-                            value={currentEvent.date}
-                          />
-                        </div>
-                        <div className="form-group mb-3">
-                          <label>Hora</label>
-                          <input
-                            type={"time'"}
-                            className="form-control"
-                            value={currentEvent.hour}
-                          />
-                        </div>
-                        <div className="form-group mb-3">
-                          <label>Ubicación</label>
-                          <input
-                            type={"text"}
-                            className="form-control"
-                            value={"Rionegro"}
-                          />
-                        </div>
-                        <div className="form-group mb-3">
-                          <button className="btn btn-info" type="submit">
-                            Editar
-                          </button>
-                        </div>
-                      </form>
-                    </div>
-                    <div class="modal-footer"></div>
-                  </div>
-                </div>
-              </div>
 
-              <div class="modal text-start" id="newModal">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header text-center">
-                      <h5 class="modal-title">Evently</h5>
-                      <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Close"
-                      ></button>
-                    </div>
-                    <div class="modal-body">
-                      <form>
-                        <div className="form-group mb-3">
-                          <label>Title</label>
-                          <input type={"text"} className="form-control" />
-                        </div>
-                        <div className="form-group mb-3">
-                          <label>Categoría</label>
-                          <input type={"text"} className="form-control" />
-                        </div>
-                        <div className="form-group mb-3">
-                          <label>Descripción</label>
-                          <input type={"text"} className="form-control" />
-                        </div>
-                        <div className="form-group mb-3">
-                          <label>Fecha</label>
-                          <input
-                            type={"date"}
-                            className="form-control"
-                            disabled
-                            value={newEvent.date}
-                          />
-                        </div>
-                        <div className="form-group mb-3">
-                          <label>Hora</label>
-                          <input type={"time'"} className="form-control" />
-                        </div>
-                        <div className="form-group mb-3">
-                          <label>Ubicación</label>
-                          <input type={"text"} className="form-control" />
-                        </div>
-                        <div className="form-group mb-3">
-                          <button className="btn btn-info" type="submit">
-                            Crear evento
-                          </button>
-                        </div>
-                      </form>
-                    </div>
-                    <div class="modal-footer"></div>
-                  </div>
+          <div class="modal text-start" id="newModal">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header text-center">
+                  <h5 class="modal-title">Evently</h5>
+                  <button
+                    type="button"
+                    class="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
                 </div>
+                <div class="modal-body">
+                  <form>
+                    <div className="form-group mb-3">
+                      <label>Title</label>
+                      <input type={"text"} className="form-control" />
+                    </div>
+                    <div className="form-group mb-3">
+                      <label>Categoría</label>
+                      <input type={"text"} className="form-control" />
+                    </div>
+                    <div className="form-group mb-3">
+                      <label>Descripción</label>
+                      <input type={"text"} className="form-control" />
+                    </div>
+                    <div className="form-group mb-3">
+                      <label>Fecha</label>
+                      <input
+                        type={"date"}
+                        className="form-control"
+                        disabled
+                        value={newEvent.date}
+                      />
+                    </div>
+                    <div className="form-group mb-3">
+                      <label>Hora</label>
+                      <input type={"time'"} className="form-control" />
+                    </div>
+                    <div className="form-group mb-3">
+                      <label>Ubicación</label>
+                      <input type={"text"} className="form-control" />
+                    </div>
+                    <div className="form-group mb-3">
+                      <button className="btn btn-info" type="submit">
+                        Crear evento
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div class="modal-footer"></div>
               </div>
+            </div>
           </div>
-          <div className="col-2 align-self-end">
-
-          <img src={avatar} width="250px"  alt="asistente" id="avatar"  className="avatar"/>
-          </div>
+        </div>
+        <div className="col-2 align-self-end">
+          <img
+            src={avatar}
+            width="250px"
+            alt="asistente"
+            id="avatar"
+            className="avatar"
+          />
+        </div>
       </div>
     </div>
   );
